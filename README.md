fb06da07a3a682ece42f384f76a5a6db3b18f8be
Jason Shepherd <jbshep@gmail.com>
2023-09-08 14:54:50 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # pydev
+
+BVU students can use this repository to build their own quick Python development environment quickly.  By running the command below, the import script will accomplish the following:
+
+1. Setup a virtual environment.
+2. Install a Python toolset for linting, type-checking, and unit testing.
+3. Create a Makefile to automatically run all of the tools in #2.
+
+## Usage
+
+To install to your development directory, type the following.
+
+```
+curl https://raw.githubusercontent.com/jbshep/pydev/main/import.sh | bash
+```
+
+All verification checks can be run using the command `make all`.
+
+## Current Tools
+
+Tools currently installed by the `import.sh` script are `black`, `mypy`, `pylint`, and `pytest`.  The script generates a Makefile and a pylint configuration file.
---
+++ import.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+PYTHON_EXEC=python3
+
+which python3 >/dev/null
+if [[ $? -eq 0 ]]; then
+    PYTHON_EXEC=python3
+else
+    which python >/dev/null
+    if [[ $? -eq 0 ]]; then
+        PYTHON_EXEC=python
+    else
+        echo "No python or python3 found. Exiting... sorry!"
+        exit 1
+    fi
+fi
+
+mkdir src
+mkdir tests
+$PYTHON_EXEC -m venv env
+
+# Is this Mac/Linux or Windows?
+BIN_DIR=env/bin
+if [[ -d env/Scripts ]]; then
+    BIN_DIR=env/Scripts
+fi
+
+source $BIN_DIR/activate && pip install black mypy pytest pylint && pip freeze >requirements.txt
+
+cat >.pylintrc <<EOPL
+[DESIGN]
+disable=missing-function-docstring,missing-module-docstring
+argument-rgx=[a-z\_][a-z0-9_]{0,39}$
+EOPL
+
+cat >Makefile <<EOM
+.PHONY all
+all:
+	codestyle typecheck lint test
+
+.PHONY codestyle
+codestyle:
+	black -l 79 src/ tests/
+
+.PHONY typecheck
+typecheck:
+	mypy --ignore-missing-imports src/
+
+.PHONY lint
+lint:
+	pylint src/
+
+.PHONY test
+test:
+	pytest tests/
+
+.PHONY clean
+clean:
+	find . -type f -name "*.pyc" | xargs rm -fr
+	find . -type d -name __pycache__ | xargs rm -fr
+EOM
+
+echo "******************** Environment created! **********************"
+echo "**** Use 'source env/bin/activate to enter new environment. ****"
